db1148e044781ce52a725eae85edcf066efcfda0
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2024-01-06 21:35:50 -0300

    Implementando Ofertas do Dia

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,7 +1,25 @@
 describe('home page', () => {
     it('site deve estar online', () => {
-        // cy.viewport(1440, 900)
+        // visita a pagina
         cy.visit('https://www.amazon.com.br/')
         cy.get('[class*="a-carousel-rounded-buttons"]')
+
+        // valida atalho amazon
+        cy.get('#nav-logo-sprites').then(($logoSprites) => {
+            if ($logoSprites.length > 0 && $logoSprites.is(':visible')) {
+                cy.wrap($logoSprites).click()
+            } else {
+                cy.get('#nav-bb-logo').click()
+            }
+        })
+
+        // valida se esta na pagina inicial  
+        cy.get('#gw-card-layout')
+
+        // valida caixa "ofertas do dia"
+        cy.contains('Ofertas do Dia, de A a Z')
+        cy.contains('Veja todas as ofertas').click()
+        cy.get('[class$="a-spacing-micro a-text-bold"]')
+
     })
 })
