08d0e0ff23f1b789f56898f7b375c5e0955c8d6a
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2024-01-09 20:02:58 -0300

    Adicionado subopcoes para ofertas

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -5,13 +5,11 @@ describe('home page', () => {
         cy.get('[class*="a-carousel-rounded-buttons"]')
 
         // valida atalho amazon
-        cy.get('#nav-logo-sprites').then(($logoSprites) => {
-            if ($logoSprites.length > 0 && $logoSprites.is(':visible')) {
-                cy.wrap($logoSprites).click()
-            } else {
-                cy.get('#nav-bb-logo').click()
-            }
-        })
+        if (cy.get('#nav-logo-sprites').should('exist')) {
+            cy.get('#nav-logo-sprites').click()
+        } else {
+            cy.get('#nav-bb-logo').click()
+        }
 
         // valida se esta na pagina inicial  
         cy.get('#gw-card-layout')
@@ -27,5 +25,47 @@ describe('home page', () => {
         // valida se existe oferta ativa
         cy.get('#anonCarousel1 > .a-carousel > [aria-posinset="1"] > [data-csa-c-type="item"] > .a-link-normal > .a-row > ._discount-asin-shoveler_style_badgeMessage__1IC2v > .a-size-mini > span')
             .should('have.text', 'Oferta')
+
+        // valida subopcoes de atalho de pesquisa
+        cy.contains('Todas as ofertas')
+        cy.get('[class="GridPresets-module__gridPresetsLargeItem_2xPgV2VoJCncjGPj18in1h GridPresets-module__selectedPreset_-JsFklJdPGF-wQYPAy4H2"]')
+            .scrollIntoView().should('have.text', 'Todas as ofertas')
+
+        cy.contains('Ofertas Exclusivas Prime')
+        cy.get('[data-testid="carousel-PrimeOnly"] > .GridPresets-module__gridPresetsLargeItem_2xPgV2VoJCncjGPj18in1h')
+            .scrollIntoView().should('have.text', 'Ofertas Exclusivas Prime')
+
+        cy.contains('Dispositivos Amazon e Acessórios')
+        cy.get('[data-testid="carousel-deals-collection-devices"] > .GridPresets-module__gridPresetsLargeItem_2xPgV2VoJCncjGPj18in1h')
+            .scrollIntoView().should('have.text', 'Dispositivos Amazon e Acessórios')
+
+        cy.contains('Alimentos e Bebidas')
+        cy.get('[data-testid="carousel-deals-collection-grocery"] > .GridPresets-module__gridPresetsLargeItem_2xPgV2VoJCncjGPj18in1h')
+            .scrollIntoView().should('have.text', 'Alimentos e Bebidas')
+
+        cy.contains('Automotivo')
+        cy.get('[data-testid="carousel-deals-collection-automotive"] > .GridPresets-module__gridPresetsLargeItem_2xPgV2VoJCncjGPj18in1h')
+            .scrollIntoView().should('have.text', 'Automotivo')
+
+        cy.contains('Bebês')
+        cy.get('[data-testid="carousel-deals-collection-baby"] > .GridPresets-module__gridPresetsLargeItem_2xPgV2VoJCncjGPj18in1h')
+            .scrollIntoView().should('have.text', 'Bebês')
+
+        cy.contains('Beleza')
+        cy.get('[data-testid="carousel-deals-collection-beauty"] > .GridPresets-module__gridPresetsLargeItem_2xPgV2VoJCncjGPj18in1h')
+            .scrollIntoView().should('have.text', 'Beleza')
+
+        cy.contains('Brinquedos e Jogos')
+        cy.get('[data-testid="carousel-deals-collection-toys-and-games"] > .GridPresets-module__gridPresetsLargeItem_2xPgV2VoJCncjGPj18in1h')
+            .scrollIntoView().should('have.text', 'Brinquedos e Jogos')
+
+        cy.contains('Casa')
+        cy.get('[data-testid="carousel-deals-collection-home"] > .GridPresets-module__gridPresetsLargeItem_2xPgV2VoJCncjGPj18in1h')
+            .scrollIntoView().should('have.text', 'Casa')
+
+        cy.contains('Cozinha')
+        cy.get('[data-testid="carousel-deals-collection-kitchen"] > .GridPresets-module__gridPresetsLargeItem_2xPgV2VoJCncjGPj18in1h')
+            .scrollIntoView().should('have.text', 'Cozinha')
+
     })
 })
